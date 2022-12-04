f37057673d0613ca73723de33b623741f57589e3
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-12-04 17:31:16 -0500

    :construction: Add DomElements

--- assets/js/utils/domElements.js
+++ assets/js/utils/domElements.js
@@ -1,1 +1,3 @@
 export const filters = document.body.getElementsByClassName("filters-container-list_item");
+export const containerCards =   document.getElementById("cardsContainer");
+export const containerPagination =   document.getElementById("paginationContainer");
