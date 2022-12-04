35b2cb6b020ee3392a87c125715795820e77f3cc
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-12-04 14:48:07 -0500

    :construction: temporary data test functionalities

---
+++ assets/data.js
@@ -0,0 +1,92 @@
+const postsData = [
+    {
+        id: 1,
+        title: "Soy el título",
+        subtitle: "Soy el subtitle",
+        body: "Les hablaré sobre asadasd",
+        author: "Pepito",
+        dateCreation: "2022-12-03",
+        likes: 5,
+        tags: "gastronomy"
+
+    },
+    {
+        id: 2,
+        title: "Soy el título 2",
+        subtitle: "Soy el subtitle 2",
+        body: "Les hablaré sobre asadasd 2",
+        author: "Pepito 2",
+        dateCreation: "2022-12-04",
+        likes: 10,
+        tags: "entertainment"
+
+    },
+    {
+        id: 3,
+        title: "Soy el título 3",
+        subtitle: "Soy el subtitle 3",
+        body: "Les hablaré sobre asadasd 3",
+        author: "Pepito 3",
+        dateCreation: "2022-12-05",
+        likes: 11,
+        tags: "adventure"
+
+    },
+    {
+        id: 4,
+        title: "Soy el título 4",
+        subtitle: "Soy el subtitle 4",
+        body: "Les hablaré sobre asadasd 4",
+        author: "Pepito 4",
+        dateCreation: "2022-12-06",
+        likes: 12,
+        tags: "culture"
+
+    },
+    {
+        id: 5,
+        title: "Soy el título 5",
+        subtitle: "Soy el subtitle 5",
+        body: "Les hablaré sobre asadasd 5",
+        author: "Pepito 5",
+        dateCreation: "2022-12-07",
+        likes: 13,
+        tags: "nature"
+
+    },
+    {
+        id: 6,
+        title: "Soy el título 6",
+        subtitle: "Soy el subtitle 6",
+        body: "Les hablaré sobre asadasd 6",
+        author: "Pepito 6",
+        dateCreation: "2022-12-03",
+        likes: 14,
+        tags: "culture"
+
+    },
+    {
+        id: 7,
+        title: "Soy el título 7",
+        subtitle: "Soy el subtitle 7",
+        body: "Les hablaré sobre asadasd 7",
+        author: "Pepito 7",
+        dateCreation: "2022-12-03",
+        likes: 15,
+        tags: "gastronomy"
+
+    },
+    {
+        id: 2,
+        title: "Soy el título 8",
+        subtitle: "Soy el subtitle 8",
+        body: "Les hablaré sobre asadasd 8",
+        author: "Pepito 8",
+        dateCreation: "2022-12-03",
+        likes: 16,
+        tags: "entertainment"
+
+    },
+];
+
+export {postsData};
