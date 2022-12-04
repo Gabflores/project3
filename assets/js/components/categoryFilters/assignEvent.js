e09b304c65eae30af465601c9e679cbfbaf2ccdf
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-12-04 17:35:56 -0500

    :construction: Function assign event for each filter

---
+++ assets/js/components/categoryFilters/assignEvent.js
@@ -0,0 +1,14 @@
+import handleFilter  from "./handleFilter.js";
+
+
+const assignEvent = (arryfilters) =>  {
+    arryfilters.map ( filter => {
+    const nameCategory = filter.id;
+     
+       filter.onclick = () => handleFilter (nameCategory);
+
+    });
+
+};
+
+export default assignEvent;
