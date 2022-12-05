6af1f1830ba338d3fa0c3e4682e362a254405b85
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-12-04 21:32:44 -0500

    :construction: Call pagination

--- assets/js/components/categoryFilters/filterbyCategory.js
+++ assets/js/components/categoryFilters/filterbyCategory.js
@@ -1,11 +1,14 @@
 import {postsData} from "../../../data.js";
-
-const filteredList = (value) => {
+import calculatePages from "../pagination/calculatepages.js";
+const filterList = (value) => {
     let filteredPosts = postsData.filter ((post)=>{
         return post.tags === value;
     });
     console.log(filteredPosts);
+    calculatePages(filteredPosts);
+    
     return filteredPosts;
+    
 };
 
-export default filteredList;
+export default filterList;
