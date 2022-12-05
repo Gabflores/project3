fbc91fd5745d9c9dd674bd63bd8abc1f541c4247
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-12-05 11:10:07 -0500

    :construction: Create function request

---
+++ assets/js/components/categoryFilters/request.js
@@ -0,0 +1,33 @@
+const myRequest = async () => {
+    let uri = 'http://localhost:3000/postsData';
+    const request = await fetch(uri);
+    const resultList = await request.json();
+
+
+    return(resultList);
+}
+
+export default myRequest;
+
+
+
+
+/* 
+export function makeRequest() {
+    logData();
+  
+    async function myRequest() {
+      const request = await fetch("http://localhost:3000/postsData");
+      const result = await request.json();
+      const resultList = await result.response.results;
+      return resultList;
+    }
+  
+    async function logData() {
+      const data = await myRequest();
+      //showResults(data);
+      calculate(data);
+      return data;
+    }
+}
+ */
