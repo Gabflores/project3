869e54dff1d1f12832881d3738bb9271fd9e3bf8
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-12-05 23:41:09 -0500

    :construction: Call function show details

--- assets/js/components/pageDetails/details.js
+++ assets/js/components/pageDetails/details.js
@@ -1,9 +1,26 @@
+import showDetails from "./showDetails.js";
+
 const id = new URLSearchParams(window.location.search).get('id');
 
-const showDetails = async () => {
+const requestDetails = async () => {
     const response = await fetch('http://localhost:3000/postsData/' + id);
     const post = await response.json();
-    console.log(post)
-}
 
-window.addEventListener('DOMContentLoaded', () => showDetails());
+    const titlePost = post.title;
+    const subtitlePost = post.subtitle;
+    const bodyPost = post.body;
+    const authorPost = post.author;
+    const datePost = post.dateCreation;
+    const likesPost = post.likes;
+    const tagsPost = post.tags;
+    const imageUrlPost = post.imageUrl;
+
+    console.log(likesPost);
+
+
+    showDetails(titlePost,subtitlePost,bodyPost,authorPost,datePost,likesPost,tagsPost,imageUrlPost);
+  }
+
+window.addEventListener('DOMContentLoaded', () => requestDetails());
+
+export default requestDetails;
