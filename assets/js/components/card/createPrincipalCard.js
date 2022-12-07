1ed644c4a00f05bcd6960d26ab35c07097407398
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-12-07 16:52:40 -0500

    :construction: Create cards principal section

---
+++ assets/js/components/card/createPrincipalCard.js
@@ -0,0 +1,99 @@
+//import {placeholderimage} from "../../../images/placeholderimage.png";
+
+const createPrincipalCard = (
+  titleResult,
+  subtitleResult,
+  bodyResult,
+  authorResult,
+  dateResult,
+  likesResult,
+  tagsResult,
+  imgUrlResult,
+  container
+) => {
+  const cardBox = document.createElement("div");
+  cardBox.classList.add("cardscontainer-card");
+  //cardBox.classList.add("cardscontainer-card");
+  //cardBox.setAttribute("id","card"+container);
+
+  const containerAuthor = document.createElement("div");
+  containerAuthor.classList.add("card-containerauthor");
+
+  const authorCard = document.createElement("p");
+  authorCard.classList.add("card-containerauthor__author");
+  authorCard.append(authorResult);
+
+  const titleCard = document.createElement("p");
+  titleCard.classList.add("card__title");
+  titleCard.append(titleResult);
+
+  const subtitleCard = document.createElement("p");
+  subtitleCard.classList.add("card__subtitle");
+  subtitleCard.append(subtitleResult);
+
+  const containerDate = document.createElement("div");
+  containerDate.classList.add("card-containerdate");
+
+  const dateCard = document.createElement("p");
+  dateCard.classList.add("card-containerdate__date");
+  dateCard.append(dateResult);
+
+  const imgCard = document.createElement("img");
+  imgCard.classList.add("card__img");
+
+  const imgSource =
+    imgUrlResult !== ""
+      ? imgUrlResult
+      : "../../../assets/images/placeholderimage.png";
+  imgCard.setAttribute("src", imgSource);
+
+  const containerInformation = document.createElement("div");
+  containerInformation.classList.add("card-containerinformation");
+
+  const containerText = document.createElement("div");
+  containerText.classList.add("card-containertext");
+
+  const containerLikes = document.createElement("div");
+  containerLikes.classList.add("card-containerlikes");
+
+  const likesCard = document.createElement("p");
+  likesCard.classList.add("card-containerlikes__likes");
+  likesCard.append(likesResult);
+
+  const containerLearn = document.createElement("div");
+  containerLearn.classList.add("card-containerlearn");
+
+  const learnCard = document.createElement("a");
+  learnCard.classList.add("card-containerLearn__learn");
+  learnCard.append("LEARN MORE ->");
+  //learnCard.setAttribute("href", urlResult);
+  learnCard.setAttribute("target", "_blank");
+  learnCard.setAttribute("rel", "noopener noreferrer");
+
+  const containerTags = document.createElement("div");
+  containerTags.classList.add("card-containertags");
+
+  const tagsCard = document.createElement("p");
+  tagsCard.classList.add("card-containertags_tags");
+  tagsCard.append(tagsResult);
+
+  cardBox.append(containerInformation);
+  containerInformation.append(imgCard);
+  containerInformation.append(containerText);
+  containerText.append(containerDate);
+  containerDate.append(dateCard);
+  containerText.append(titleCard);
+  containerText.append(subtitleCard);
+  containerText.append(containerLearn);
+  containerLearn.append(learnCard);
+  containerInformation.append(containerLikes);
+  containerLikes.append(likesCard);
+  containerInformation.append(containerTags);
+  containerTags.append(tagsCard);
+
+  //containerCards.append(cardBox);
+
+  return cardBox;
+};
+
+export default createPrincipalCard;
