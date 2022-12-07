82127aeadbc1901b08d83cf9df9c4fa160658d60
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-12-07 16:56:32 -0500

    :construction: Improve request send comments

--- assets/js/components/pageDetails/sendComment.js
+++ assets/js/components/pageDetails/sendComment.js
@@ -1,26 +1,33 @@
+import { getPost, getPostComments } from "../../../../services/post.service.js";
+
 const fieldAuthor = document.getElementById("newCommentAuthor");
 const fieldBody = document.getElementById("newCommentInput");
 //const fieldDate = document.getElementById("commentDate");
 
 const sendComment = async (id) => {
-  //e.preventDefault();
-  const updatecomment = ({
-      id: id,
-    coments: {
-      authorComment: fieldAuthor.value,
-      bodyComment: fieldBody.value,
-      dateComment: "21/09/21",
-    }
-  });
-  console.log(updatecomment);
+  // e.preventDefault();
 
-  const response = await fetch("http://localhost:3000/postsData", {
-  method: "PUT",
-  body: JSON.stringify(updatecomment),
-   headers: {
-    "Content-Type": 'application/json'
-  },  
-});
-}
+  const previousComments = await getPostComments(id);
+  console.log(previousComments);
+  const updatedComments = {
+    coments: [
+      ...previousComments,
+
+      {
+        authorComment: fieldAuthor.value,
+        bodyComment: fieldBody.value,
+        dateComment: "21/09/21",
+      },
+    ],
+  };
+
+  await fetch(`http://localhost:3000/postsData/${id}`, {
+    method: "PATCH",
+    body: JSON.stringify(updatedComments),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
 
 export default sendComment;
