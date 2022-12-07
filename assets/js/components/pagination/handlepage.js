01d945ef0378bc7ca433d6ad2c9536e427073130
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-12-07 17:46:53 -0500

    :construction: Add functionality scroll

--- assets/js/components/pagination/handlepage.js
+++ assets/js/components/pagination/handlepage.js
@@ -1,15 +1,18 @@
-
 import { containerCards } from "../../utils/domElements.js";
 import cleanNode from "../../utils/clean.js";
 import showCard from "../cardContainer/showCard.js";
 
+const handlepage = (data, index) => {
+  console.log(data);
+  cleanNode(containerCards);
+  console.log("momo");
+  showCard(data.slice(index * 10 + 3, index * 10 + 13), containerCards);
+  console.log("index" + index);
 
-const handlepage = (data,index) =>{
-    console.log(data);
-    cleanNode(containerCards);
-    console.log("momo");
-    showCard(data.slice((index)*10+3,(index)*10 +13),containerCards) ;
-    console.log("index"+index);
-}
+  window.scrollTo({
+    top: 350,
+    behavior: "auto",
+  });
+};
 
 export default handlepage;
