cc596b5fb63df5dd7af699f711cd90a3eb5332ba
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-12-07 05:53:16 -0500

    :construction: Fetch send comment

--- assets/js/components/pageDetails/sendComment.js
+++ assets/js/components/pageDetails/sendComment.js
@@ -2,28 +2,32 @@ const fieldAuthor = document.getElementById("newCommentAuthor");
 const fieldBody = document.getElementById("newCommentInput");
 //const fieldDate = document.getElementById("commentDate");
 
+const sendComment = async (id) => {
+  //e.preventDefault();
 
-const sendComment = async () => {
- //e.preventDefault();
+  const newcomment = {
+    id: id,
+    coments: {
+      authorComment: fieldAuthor.value,
+      bodyComment: fieldBody.value,
+      dateComment: "21/09/21",
+    },
+  }
 
- const comment = {
-    author: fieldAuthor.value,
-    body: fieldBody.value,
-    date: "21/09/21",
- }
+  console.log("nuevo comentario");
 
-/*  await fetch('http://localhost:3000/postsData',{
- method: 'POST',lñ-
- body: JSON.stringify(comment)
+/* await fetch("http://localhost:3000/postsData", {
+  headers: {
+    "Content-Type": "application/json",
+  },
+  method: "POST",
+  body: JSON.stringify(newcomment),
+}); */
+}
 
- });
-
+/*
  let uri = 'http://localhost:3000/postsData';
  const request = await fetch(uri);
  const resultList = await request.json();
  */
-console.log(comment);
-
-};
-
 export default sendComment;
