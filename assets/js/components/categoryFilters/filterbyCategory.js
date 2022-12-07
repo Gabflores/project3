5459b498e17cc748970bf42a60c9f60333a280dd
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-12-07 16:57:10 -0500

    :construction: Change range news display

--- assets/js/components/categoryFilters/filterbyCategory.js
+++ assets/js/components/categoryFilters/filterbyCategory.js
@@ -2,26 +2,22 @@ import myRequest from "./request.js";
 import calculatePages from "../pagination/calculatepages.js";
 import showCard from "../cardContainer/showCard.js";
 import { containerCards } from "../../utils/domElements.js";
-import {principalNews} from "../../utils/domElements.js";
+import { principalNews } from "../../utils/domElements.js";
 import cleanNode from "../../utils/clean.js";
 
 const filterList = async (value) => {
+  let listPosts = await myRequest();
+  let filteredPosts = listPosts.filter((post) => {
+    return post.tags === value;
+  });
+  console.log(filteredPosts);
+  calculatePages(filteredPosts);
+  cleanNode(containerCards);
+  showCard(filteredPosts.slice(3, 13), containerCards, "normal");
+  cleanNode(principalNews);
+  showCard(filteredPosts.slice(0, 3), principalNews, "principal");
 
-    let listPosts = await myRequest();
-    let filteredPosts = listPosts.filter ((post)=>{
-        return post.tags === value;
-    });
-    console.log(filteredPosts);
-    calculatePages(filteredPosts);
-    cleanNode(containerCards);
-    showCard(filteredPosts.slice(3,13),containerCards) ;
-    cleanNode(principalNews);
-    showCard(filteredPosts.slice(0,3),principalNews) ;
-
-
-   
-    return filteredPosts;
-    
+  return filteredPosts;
 };
 
 export default filterList;
