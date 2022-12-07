7bf5df956edff4d7d27fcfe42f4871ce19f28999
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-12-06 22:15:28 -0500

    :fire: Transfer code another file

--- assets/js/components/pageDetails/details.js
+++ assets/js/components/pageDetails/details.js
@@ -1,26 +1,11 @@
-import showDetails from "./showDetails.js";
+import assingHandlerComment from "./assignhandleComment.js";
+import requestDetails from "./requestDetails.js";
 
-const id = new URLSearchParams(window.location.search).get('id');
+const id = new URLSearchParams(window.location.search).get("id");
 
-const requestDetails = async () => {
-    const response = await fetch('http://localhost:3000/postsData/' + id);
-    const post = await response.json();
-
-    const titlePost = post.title;
-    const subtitlePost = post.subtitle;
-    const bodyPost = post.body;
-    const authorPost = post.author;
-    const datePost = post.dateCreation;
-    const likesPost = post.likes;
-    const tagsPost = post.tags;
-    const imageUrlPost = post.imageUrl;
-
-    console.log(likesPost);
+window.addEventListener("DOMContentLoaded", () =>
+{ requestDetails(id);
+  assingHandlerComment(id);
+});
 
 
-    showDetails(titlePost,subtitlePost,bodyPost,authorPost,datePost,likesPost,tagsPost,imageUrlPost);
-  }
-
-window.addEventListener('DOMContentLoaded', () => requestDetails());
-
-export default requestDetails;
