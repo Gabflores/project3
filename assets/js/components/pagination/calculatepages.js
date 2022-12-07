2ed3136a9dafbf1662739712c86086e3d158d724
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-12-07 10:06:03 -0500

    :construction: Change the number of news

--- assets/js/components/pagination/calculatepages.js
+++ assets/js/components/pagination/calculatepages.js
@@ -3,7 +3,7 @@ import createdivspages from "./createdivpages.js";
 const calculatePages = (result) => {
 
     let numPages = "";
-    (result.length % 10 ===0) ? (numPages =  Math.trunc(result.length/10)) : (numPages =  Math.trunc(result.length/10) + 1);
+    (result.length % 10 ===0) ? (numPages =  Math.trunc((result.length-3)/10)) : (numPages =  Math.trunc((result.length-3)/10) + 1);
     createdivspages(result, numPages);
    console.log(result.length/10);
     console.log(numPages);
