d0fed92ce1aa49b62c608a5e6812a0cb7aaf1e99
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-12-07 10:11:16 -0500

    :construction: Get Element section principal news

--- assets/js/utils/domElements.js
+++ assets/js/utils/domElements.js
@@ -1,3 +1,4 @@
 export const filters = document.body.getElementsByClassName("filters-container-list_item");
 export const containerCards =   document.getElementById("cardsContainer");
 export const containerPagination =   document.getElementById("paginationContainer");
+export const principalNews = document.getElementById("principalNews");
