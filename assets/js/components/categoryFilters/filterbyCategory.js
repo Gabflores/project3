dac7b0db34aec795c966de5ec696f9aad30c6752
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-12-07 10:08:44 -0500

    :construction: Add section Principal News

--- assets/js/components/categoryFilters/filterbyCategory.js
+++ assets/js/components/categoryFilters/filterbyCategory.js
@@ -1,5 +1,10 @@
 import myRequest from "./request.js";
 import calculatePages from "../pagination/calculatepages.js";
+import showCard from "../cardContainer/showCard.js";
+import { containerCards } from "../../utils/domElements.js";
+import {principalNews} from "../../utils/domElements.js";
+import cleanNode from "../../utils/clean.js";
+
 const filterList = async (value) => {
 
     let listPosts = await myRequest();
@@ -8,7 +13,13 @@ const filterList = async (value) => {
     });
     console.log(filteredPosts);
     calculatePages(filteredPosts);
-    
+    cleanNode(containerCards);
+    showCard(filteredPosts.slice(3,13),containerCards) ;
+    cleanNode(principalNews);
+    showCard(filteredPosts.slice(0,3),principalNews) ;
+
+
+   
     return filteredPosts;
     
 };
