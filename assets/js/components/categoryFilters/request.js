08a97d685b6c1b4a70308f599c53e3bd9af62e8e
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-12-07 10:07:58 -0500

    :construction: Results Ordered desc

--- assets/js/components/categoryFilters/request.js
+++ assets/js/components/categoryFilters/request.js
@@ -1,5 +1,5 @@
 const myRequest = async () => {
-    let uri = 'http://localhost:3000/postsData';
+    let uri = 'http://localhost:3000/postsData?_sort=dateCreation&_order=desc';
     const request = await fetch(uri);
     const resultList = await request.json();
 
