aa7b4f9c3e70e3dc150f1fe14f4f9d94fd40864a
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-12-07 16:54:08 -0500

    :construction: Add handle of two types of cards

--- assets/js/components/cardContainer/showCard.js
+++ assets/js/components/cardContainer/showCard.js
@@ -1,30 +1,52 @@
 import createCard from "../card/createCard.js";
+import createPrincipalCard from "../card/createPrincipalCard.js";
 import handleCard from "../card/handleCard.js";
 //import { containerCards} from "../../utils/domElements.js";
 //import {principalNews} from "../../utils/domElements.js";
 
-export const showCard = (pagelist,container) => {
-    pagelist.map(element => {
-      const titleDetail = element.title;
-      const subtitleDetail = element.subtitle;
-      const bodyDetail = element.body;
-      const authorDetail = element.author;
-      const dateDetail = element.dateCreation;
-      const likesDetail = element.likes;
-      const tagsDetail = element.tags;
-      const imageUrlDetail = element.imageUrl;
+export const showCard = (pagelist, container, cardType) => {
+  pagelist.map((element) => {
+    const titleDetail = element.title;
+    const subtitleDetail = element.subtitle;
+    const bodyDetail = element.body;
+    const authorDetail = element.author;
+    const dateDetail = element.dateCreation;
+    const likesDetail = element.likes;
+    const tagsDetail = element.tags;
+    const imageUrlDetail = element.imageUrl;
 
-      
+    let newCard;
+    if (cardType === "principal") {
+      newCard = createPrincipalCard(
+        titleDetail,
+        subtitleDetail,
+        bodyDetail,
+        authorDetail,
+        dateDetail,
+        likesDetail,
+        tagsDetail,
+        imageUrlDetail,
+        container
+      );
+    } else {
+      newCard = createCard(
+        titleDetail,
+        subtitleDetail,
+        bodyDetail,
+        authorDetail,
+        dateDetail,
+        likesDetail,
+        tagsDetail,
+        imageUrlDetail,
+        container
+      );
+    }
 
-     let newcard = createCard(titleDetail, subtitleDetail, bodyDetail, authorDetail,dateDetail,likesDetail,tagsDetail,imageUrlDetail,container);
-     console.log("newcard");
-     container.append(newcard); 
-     
-     newcard.onclick = () => handleCard (element.id);
+    console.log("newcard");
+    container.append(newCard);
 
-      
+    newCard.onclick = () => handleCard(element.id);
+  });
+};
 
-    })
-  };
-
-  export default showCard;
+export default showCard;
