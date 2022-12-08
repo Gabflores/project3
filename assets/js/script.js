f8e22545ce694690f925eedc430813eac8e2dd9a
Gabflores <84301523+Gabflores@users.noreply.github.com>
2022-12-08 00:50:14 -0500

    :construction: Add debounce

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,25 @@
 import assignEvent from "./components/categoryFilters/assignEvent.js";
 import { arryfilters } from "./components/categoryFilters/getFilters.js";
-
+import searchPost from "./components/search/search.js";
+import { queryPost } from "./utils/domElements.js";
 
 assignEvent(arryfilters);
 
+const debounce = (fn, delay) => {
+  let timeoutID;
+  return function () {
+    if (timeoutID) {
+      clearTimeout(timeoutID);
+    }
+    timeoutID = setTimeout(() => {
+      fn();
+    }, delay);
+  };
+};
 
-
+queryPost.addEventListener(
+  "keyup",
+  debounce((e) => {
+    searchPost();
+  }, 2000)
+);
